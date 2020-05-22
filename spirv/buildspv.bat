1601735330db57614f002c3ea5da1209b3d6a4fe
Justin Marshall <justinmarshall20@gmail.com>
2020-05-21 17:25:39 -0700

    Added buildshader.hlsl to vulkan visual studio project.

--- spirv/buildspv.bat
+++ spirv/buildspv.bat
@@ -1,3 +1,5 @@
-glslc -fentry-point=VSMain -fshader-stage=vertex ../renderprogs/d3d12/buildshader.hlsl -o ../renderprogs/spirv/buildshader_v.spv
-glslc -fentry-point=PSMain -fshader-stage=fragment ../renderprogs/d3d12/buildshader.hlsl -o ../renderprogs/spirv/buildshader_p.spv
-pause
+@echo off
+echo Building Vertex Shader
+glslc -fentry-point=VSMain -fshader-stage=vertex ../../renderprogs/d3d12/buildshader.hlsl -o ../../renderprogs/spirv/buildshader_v.spv
+echo Building Fragment Shader
+glslc -fentry-point=PSMain -fshader-stage=fragment ../../renderprogs/d3d12/buildshader.hlsl -o ../../renderprogs/spirv/buildshader_p.spv
